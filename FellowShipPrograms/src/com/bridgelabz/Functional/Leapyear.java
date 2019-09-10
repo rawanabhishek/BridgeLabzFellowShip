c376bb82b7f60414ae94a1e0d8a93e5f1898da68
rawanabhishek <rawatabhishek2012@gmail.com>
2019-09-10 10:24:05 +0530

    Leap Year Functinoal Program

---
+++ FellowShipPrograms/src/com/bridgelabz/Functional/Leapyear.java
@@ -0,0 +1,29 @@
+package com.bridgelabz.Functional;
+
+import java.util.Scanner;
+
+import com.bridgelabz.Utility.FunctionalUtility;
+
+public class Leapyear {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		FunctionalUtility utility = new FunctionalUtility();
+		
+		System.out.println("Enter a year to check it is Leap Year or Not ");
+		int year=scanner.nextInt();
+		if(year>1582 ) {
+			if(utility.Leapyear(year)){
+				System.out.println("The "+year+" is a leap year");
+			}
+			else {
+				System.out.println("The "+year+" is not  a leap year");
+			}
+		}
+		
+		scanner.close();
+	
+
+	}
+
+}
--- FellowShipPrograms/src/com/bridgelabz/Utility/FunctionalUtility.java
+++ FellowShipPrograms/src/com/bridgelabz/Utility/FunctionalUtility.java
@@ -9,9 +9,9 @@
  ******************************************************************************/
 package com.bridgelabz.Utility;
 
-public class FunctionalUtility {
+public  class FunctionalUtility {
 
-	
+	 /******************************************************************************/
 	//to replace the name in the given string
 	public void ReplaceName(String str, String name) {
 		
@@ -24,6 +24,51 @@ public class FunctionalUtility {
 	       
 	       System.out.println(RecString);
 		}
+	
+	/******************************************************************************/
+
+	//to flip a coin n number of times and give the percentage of head and tail
+	public   String Coin(int flip) {
+		int head = 0 ,tail =0 ;
+        for(int i=0 ; i<flip ;i++) {
+        	double flipping=Math.random();
+        	
+        	if(flipping<= 0.5) {
+        		 System.out.println(" Heads ");
+        		head++;
+        	}
+        	else {
+        		System.out.println(" Tails");;
+        		tail++;
+        	}
+        	
+        }
+        
+        double averagehead = (int)head*100/flip;
+       
+		double averagetail = (int)tail*100/flip;
+        
+      return "Percentage of Head is "+averagehead+ " Percentage of tail is "+averagetail ;
+	}
+	
+	/******************************************************************************/
+
+	/*  Purpose: to Check the given year is a leap year or not
+     * @param year  take input from user to check the leap year
+     * @return      return true if it is leap year else return false
+     */
+
+     public Boolean Leapyear(int year) {
+    	 if(year%4==0 && year%100!=0) {
+    		 return true ;
+    	 }
+    	 else if(year%4==0 && year%400==0) {
+    		 return true ;
+    	 }
+    	 else {
+    		 return false;
+    	 }
+     }
 	
 
 }
