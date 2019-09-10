33b7d93a958fae81e706d000fc6ca91856697bc2
rawanabhishek <rawatabhishek2012@gmail.com>
2019-09-10 10:29:44 +0530

    PowerOf2  Functinoal Program

--- FellowShipPrograms/src/com/bridgelabz/Utility/FunctionalUtility.java
+++ FellowShipPrograms/src/com/bridgelabz/Utility/FunctionalUtility.java
@@ -9,66 +9,79 @@
  ******************************************************************************/
 package com.bridgelabz.Utility;
 
-public  class FunctionalUtility {
+public class FunctionalUtility {
 
-	 /******************************************************************************/
-	//to replace the name in the given string
+	/******************************************************************************/
+	// to replace the name in the given string
 	public void ReplaceName(String str, String name) {
-		
-		String RecString="";
-	       
-	       if(str.length()>3) {
-	    	   RecString =str.replace("<<UserName>>", name);
-	    	   
-	       }
-	       
-	       System.out.println(RecString);
+
+		String RecString = "";
+
+		if (str.length() > 3) {
+			RecString = str.replace("<<UserName>>", name);
+
 		}
-	
-	/******************************************************************************/
 
-	//to flip a coin n number of times and give the percentage of head and tail
-	public   String Coin(int flip) {
-		int head = 0 ,tail =0 ;
-        for(int i=0 ; i<flip ;i++) {
-        	double flipping=Math.random();
-        	
-        	if(flipping<= 0.5) {
-        		 System.out.println(" Heads ");
-        		head++;
-        	}
-        	else {
-        		System.out.println(" Tails");;
-        		tail++;
-        	}
-        	
-        }
-        
-        double averagehead = (int)head*100/flip;
-       
-		double averagetail = (int)tail*100/flip;
-        
-      return "Percentage of Head is "+averagehead+ " Percentage of tail is "+averagetail ;
+		System.out.println(RecString);
 	}
-	
+
 	/******************************************************************************/
 
-	/*  Purpose: to Check the given year is a leap year or not
-     * @param year  take input from user to check the leap year
-     * @return      return true if it is leap year else return false
-     */
+	// to flip a coin n number of times and give the percentage of head and tail
+	public String Coin(int flip) {
+		int head = 0, tail = 0;
+		for (int i = 0; i < flip; i++) {
+			double flipping = Math.random();
 
-     public Boolean Leapyear(int year) {
-    	 if(year%4==0 && year%100!=0) {
-    		 return true ;
-    	 }
-    	 else if(year%4==0 && year%400==0) {
-    		 return true ;
-    	 }
-    	 else {
-    		 return false;
-    	 }
-     }
-	
+			if (flipping <= 0.5) {
+				System.out.println(" Heads ");
+				head++;
+			} else {
+				System.out.println(" Tails");
+				;
+				tail++;
+			}
+
+		}
+
+		double averagehead = (int) head * 100 / flip;
+
+		double averagetail = (int) tail * 100 / flip;
+
+		return "Percentage of Head is " + averagehead + " Percentage of tail is " + averagetail;
+	}
+
+	/******************************************************************************/
+
+	/*
+	 * Purpose: to Check the given year is a leap year or not
+	 * 
+	 * @param year take input from user to check the leap year
+	 * 
+	 * @return return true if it is leap year else return false
+	 */
+
+	public Boolean Leapyear(int year) {
+		if (year % 4 == 0 && year % 100 != 0) {
+			return true;
+		} else if (year % 4 == 0 && year % 400 == 0) {
+			return true;
+		} else {
+			return false;
+		}
+	}
+
+	/******************************************************************************/
+	// Purpose: to print the table of Power of 2
+
+	public void Power(int number) {
+		for (int i = 0; i <= number; i++) {
+
+			int Table = (int) Math.pow(2, i);
+			System.out.println("2 ^ " + i + " =" + Table);
+
+		}
+
+	}
 
 }
