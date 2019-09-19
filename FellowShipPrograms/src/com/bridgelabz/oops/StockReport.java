e2bb4052788855f121c176959058b517010a357d
rawanabhishek <rawatabhishek2012@gmail.com>
2019-09-19 19:34:31 +0530

    oops started..

--- FellowShipPrograms/src/com/bridgelabz/oops/StockReport.java
+++ FellowShipPrograms/src/com/bridgelabz/oops/StockReport.java
@@ -20,24 +20,26 @@ public class StockReport {
 		ObjectMapper mapper = new ObjectMapper();
 		StockModel data = mapper.readValue(new File(sourcein), StockModel.class);
 		
-		System.out.println("Stock name"+data.getTata().get(0).getName());
-		System.out.println("Stock shares"+data.getTata().get(0).getShare());
-		System.out.println("Stock price"+data.getTata().get(0).getPrice());
+		System.out.println("Stock name : "+data.getTata().get(0).getName());
+		System.out.println("Stock shares : "+data.getTata().get(0).getShare());
+		System.out.println("Stock price: "+data.getTata().get(0).getPrice());
 		int valueOfTcs=OopsUtility.calculateValueOfStock(data.getTata().get(0).getShare(),data.getTata().get(0).getPrice());
 		System.out.println();
 		
 		
-		System.out.println("Stock name"+data.getHdfc().get(0).getName());
-		System.out.println("Stock shares"+data.getHdfc().get(0).getShare());
-		System.out.println("Stock price"+data.getHdfc().get(0).getPrice());
+		System.out.println("Stock name : "+data.getHdfc().get(0).getName());
+		System.out.println("Stock shares : "+data.getHdfc().get(0).getShare());
+		System.out.println("Stock price : "+data.getHdfc().get(0).getPrice());
 		int valueOfHfc=OopsUtility.calculateValueOfStock(data.getHdfc().get(0).getShare(),data.getHdfc().get(0).getPrice());
 		System.out.println();
 		
-		System.out.println("Stock name"+data.getZee().get(0).getName());
-		System.out.println("Stock shares"+data.getZee().get(0).getShare());
-		System.out.println("Stock price"+data.getZee().get(0).getPrice());
+		System.out.println("Stock name : "+data.getZee().get(0).getName());
+		System.out.println("Stock shares : "+data.getZee().get(0).getShare());
+		System.out.println("Stock price : "+data.getZee().get(0).getPrice());
 		int valueOfZee=OopsUtility.calculateValueOfStock(data.getZee().get(0).getShare(),data.getZee().get(0).getPrice());
 		
+		
+		data.setValueOfTcs
 		
 		
 	}
