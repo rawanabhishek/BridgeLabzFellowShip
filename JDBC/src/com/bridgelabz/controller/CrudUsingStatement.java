3df0365c374cac58bba46cb3bb717c79121b0f96
rawanabhishek <rawatabhishek2012@gmail.com>
2019-09-30 19:58:52 +0530

    jdbc started

---
+++ JDBC/src/com/bridgelabz/controller/CrudUsingStatement.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.controller;
+
+
+
+public class CrudUsingStatement {
+
+	public static void main(String[] args) {
+		
+		
+	}
+
+}
---
+++ JDBC/src/com/bridgelabz/service/ICrud.java
@@ -0,0 +1,8 @@
+package com.bridgelabz.service;
+
+public interface ICrud {
+	
+	public void getInput();
+	
+
+}
